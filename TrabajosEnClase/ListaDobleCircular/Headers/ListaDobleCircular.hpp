b86189b50e2884875f4d5e93a138bd049477e2e4
AlbertoAmayaSoria <albertoamayasoria@gmail.com>
2025-03-09 19:05:49 -0700

    LDC

--- TrabajosEnClase/ListaDobleCircular/Headers/ListaDobleCircular.hpp
+++ TrabajosEnClase/ListaDobleCircular/Headers/ListaDobleCircular.hpp
@@ -16,7 +16,92 @@
 template <typename T>
 class ListaDobleCircular {
 public:
-    
+
+    // Métodos indispensables para el buen funcionamiento.
+
+    /*  
+     * @brief Constructor por defefecto.
+     * Inicializa una lista vacía.
+     */
+    ListaDobleCircular();
+
+    /*
+     * @brief Constructor de copias.
+     * @param otra lista a copiar.
+     */
+    ListaDobleCircular(const ListaDobleCircular& otra);
+
+    /*
+     * @brief Destructor.
+     * Libera la memoria utilizada por la lista.
+     */
+    ~ListaDobleCircular();
+
+    /*
+     * @brief Sobrecarga del operador de asignación.
+     * @param otra lista a copiar.
+     * @return Referencia a la lista actual
+     */
+    ListaDobleCircular& operator=(const ListaDobleCircular& otra);
+
+    /*
+     * @brief Inserta un elemento en la cabeza de la lista.
+     * @param valor Valor a insertar.
+     */
+    void insertar(T valor);
+
+    /*
+     * @brief eliminar el elemento en cabeza de la lista.
+     * @throw std::out_of_range Si la lista esta vacía.
+     */
+    void eliminar();
+
+    /*
+     * @brief Buscar un valor en la lista.
+     * @param valor Valor a buscar.
+     * @return true si el valor está en la lista, false en caso contrario.
+     */
+    bool buscar(T valor) const;
+
+    /*
+     * @brief Avanza en uno la posición de la cabeza.
+     * @throw std::out_of_range Si la lista esta vacía.
+     */
+    void avanzarCabeza();
+
+    /*
+     * @brief Retrocede en uno la posición de la cabeza.
+     * @throw std::out_of_range Si la lista esta vacía.
+     */
+    void retrocederCabeza();
+
+    /*
+     * @brief Conocer el número de elementos en la lista.
+     * @return Número de elementos.
+     */
+    size_t obtenerTamaño();
+
+    /*
+     * @brief Verifica si la lista está vacía.
+     * @return true si estas vacía, false en caso constrario.
+     */
+    bool estaVacia();
+
+    /*
+     * @brief Vacía la lista.
+     */
+    void vaciar();
+
+    /*
+     * @brief Imprime la lista hacia adelante.
+     */
+    void imprimirAdelante();
+
+    /*
+     * @brief Imprime la lista hacia atras.
+     */
+    void imprimirReversa();
+
 
 private:
     /**
