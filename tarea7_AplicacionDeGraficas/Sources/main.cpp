4b1d4ab41192bd7b30e271c34a46fbd1bc91f0b0
AlbertoAmayaSoria <albertoamayasoria@gmail.com>
2025-03-23 18:28:44 -0700

    falta documentar la tarea 7

--- tarea7_AplicacionDeGraficas/Sources/main.cpp
+++ tarea7_AplicacionDeGraficas/Sources/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include "../Headers/Grafica.hpp"
 
 using namespace std;
 
+void LimpiarPantalla();
+
+void DetenerImpresion(int milisegundos);
+
 int main()
 {
+
+    LimpiarPantalla();
+
     // Grafo 1: Grafo con camino Euleriano (todos los grados pares, grafo conexo)
     Grafica g;
     g.Agregar('1');
@@ -19,8 +28,11 @@ int main()
     
     cout << "Gráfica 1 original (camino Euleriano):" << endl;
     g.Imprimir();
-    g.CaminoEuleriano();
-    
+    g.CaminoEuleriano();  // Este grafo debe tener un camino Euleriano
+   
+    DetenerImpresion(8000);
+    LimpiarPantalla();
+
     // Grafo 2: Grafo con camino Euleriano (todos los grados pares, grafo conexo)
     Grafica f;
     f.Agregar('a');
@@ -35,44 +47,31 @@ int main()
     
     cout << "\nGráfica 2 original (camino Euleriano):" << endl;
     f.Imprimir();
-    f.CaminoEuleriano();
+    f.CaminoEuleriano();  // Este grafo debe tener un camino Euleriano
     
+    DetenerImpresion(8000);
+    LimpiarPantalla();
+
     // Grafo 3: Grafo con camino Euleriano (todos los grados pares, grafo conexo)
     Grafica i;
     i.Agregar('a');
     i.Agregar('b');
     i.Agregar('c');
     i.Agregar('d');
-    i.Agregar('e');
     
     i.Agregar('a', 'b');
-    i.Agregar('a', 'c');
-    i.Agregar('b', 'd');
-    i.Agregar('d', 'c');
-    i.Agregar('c', 'a');
+    i.Agregar('b', 'c');
+    i.Agregar('c', 'd');
+    i.Agregar('d', 'a');
     
     cout << "\nGráfica 3 original (camino Euleriano):" << endl;
     i.Imprimir();
-    i.CaminoEuleriano();
+    i.CaminoEuleriano();  // Este grafo también tiene un camino Euleriano
     
-    // Grafo 4: Grafo con camino Euleriano (todos los grados pares, grafo conexo)
-    Grafica j;
-    j.Agregar('a');
-    j.Agregar('b');
-    j.Agregar('c');
-    j.Agregar('d');
-    j.Agregar('e');
-    
-    j.Agregar('a', 'b');
-    j.Agregar('b', 'd');
-    j.Agregar('d', 'c');
-    j.Agregar('c', 'a');
-    
-    cout << "\nGráfica 4 original (camino Euleriano):" << endl;
-    j.Imprimir();
-    j.CaminoEuleriano();
-    
-    // Grafo 5: Grafo sin camino Euleriano (grados impares)
+    DetenerImpresion(8000);
+    LimpiarPantalla();
+
+    // Grafo 4: Grafo sin camino Euleriano (grados impares)
     Grafica k;
     k.Agregar('1');
     k.Agregar('2');
@@ -81,46 +80,21 @@ int main()
     k.Agregar('1', '2');
     k.Agregar('2', '3');
     
-    cout << "\nGráfica 5 (sin camino Euleriano):" << endl;
+    cout << "\nGráfica 4 (sin camino Euleriano, tiene vértices con grado impar):" << endl;
     k.Imprimir();
-    k.CaminoEuleriano();
-    
-    // Grafo 6: Grafo desconectado (sin camino Euleriano)
-    Grafica l;
-    l.Agregar('1');
-    l.Agregar('2');
-    l.Agregar('3');
-    l.Agregar('4');
-    
-    l.Agregar('1', '2');
-    // No hay conexión con 3 y 4
-    
-    cout << "\nGráfica 6 (desconectada, sin camino Euleriano):" << endl;
-    l.Imprimir();
-    l.CaminoEuleriano();
-    
-    // Grafo 7: Grafo con un solo vértice
-    Grafica m;
-    m.Agregar('1');
-    
-    cout << "\nGráfica 7 (un solo vértice):" << endl;
-    m.Imprimir();
-    m.CaminoEuleriano();
-    
-    // Grafo 8: Grafo con ciclo impar (sin camino Euleriano)
-    Grafica n;
-    n.Agregar('1');
-    n.Agregar('2');
-    n.Agregar('3');
-    
-    n.Agregar('1', '2');
-    n.Agregar('2', '3');
-    n.Agregar('3', '1');
-    
-    cout << "\nGráfica 8 (ciclo impar, sin camino Euleriano):" << endl;
-    n.Imprimir();
-    n.CaminoEuleriano();
-    
+    k.CaminoEuleriano();  // Este grafo no tiene un camino Euleriano
+
     return 0;
 }
 
+void LimpiarPantalla() {
+#ifdef _WIN32
+    system("cls");  // Windows
+#else
+    system("clear");  // Linux y otros sistemas Unix
+#endif
+}
+
+void DetenerImpresion(int milisegundos) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(milisegundos));  // Pausa en milisegundos
+}
