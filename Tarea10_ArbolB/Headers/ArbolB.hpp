4ba91a49dff8eb7ba0bf5f8d51611e6527c01cc6
AlbertoAmayaSoria <albertoamayasoria@gmail.com>
2025-04-29 18:31:51 -0700

    Tarea10

--- Tarea10_ArbolB/Headers/ArbolB.hpp
+++ Tarea10_ArbolB/Headers/ArbolB.hpp
@@ -1,7 +1,7 @@
 #ifndef ARBOLB_HPP_INCLUDED
 #define ARBOLB_HPP_INCLUDED
 
-template <typename Type>
+template <typename Type, int grado = 5>
 class ArbolB {
 public:
 
@@ -11,7 +11,7 @@ public:
 
     ~ArbolB(); // Destructor
 
-    ArbolB& operator=(const Arbol &c); // Operador asignación
+    ArbolB& operator=(const ArbolB &c); // Operador asignación
 
    void Agregar(Type valor); // Agrega un nuevo elemento
 
@@ -33,51 +33,11 @@ public:
 
 private:
 
-    
+     
 
 
-}
-
-//#include 
-
-#endif // !ARBOLB_HPP_INCLUDED
-#define ARBOLB_HPP_INCLUDED
-
-template <typename Type>
-class ArbolB {
-public:
-
-    explicit ArbolB(); // Constructor por defecto
-    
-    ArbolB(const ArbolB &c); // Constrctor de copia
-
-    ~ArbolB(); // Destructor
-
-    ArbolB& operator=(const Arbol &c); // Operador asignación
-
-   void Agregar(Type valor); // Agrega un nuevo elemento
-
-    void Eliminar(Type valor); // Elimina el frente (raíz)
-
-    void BuscarElem(); // Busca un elemento en el arbol
-
-    int CantElem() const; // Devuelve la cantidad de elementos actuales
-
-    void Vaciar(); // Vacia el arbol
-
-    // Imprimir
-    void ImprimirAsc() const;
-
-    void ImprimirDes() const;
-
-    void ImprimirNiveles() const;
-
-
-private:
-
-    
-
-
-}
+};
 
 //#include 
+
+#endif // ARBOLB_HPP_INCLUDED
